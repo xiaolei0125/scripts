b2b0fd569e4f676f9e48de7f82cd959a0d056493
FrankieXiao <FrankieXiao@s3graphics.com>
2015-12-21 11:13:32 +0800

    add mysql_tomcat_start_order.sh

---
+++ docker/mysql_tomcat_start_order.sh
@@ -0,0 +1,37 @@
+﻿#!/bin/bash
+
+#docker中要注意mysql与tomcat的启动顺序，确保mysql先启动完成，可以使用：
+
+echo "Start to check mysql import"
+date
+if [ "$MYSQL_ROOT_PASSWORD" != "" ]; then
+    ROOT_PASSWD=$MYSQL_ROOT_PASSWORD
+else
+   ROOT_PASSWD=123456
+fi
+
+var1=50
+while [ $var1 -gt 1 ]
+do
+    echo "For wait mysql, out loop $var1"
+    var1=$[ $var1-1 ]
+
+    echo "use mysql" | mysql -uroot -p$ROOT_PASSWD
+    RET=$?
+    echo "Check mysql, Return is $RET"
+    if [ $RET != 0 ]; then
+         sleep 5
+         continue;
+    fi
+
+    echo "use HlPrintSvc" | mysql -uroot -p$ROOT_PASSWD
+    RET=$?
+    echo "Check HlPrintSvc, Return is $RET"
+    if [ $RET != 0 ]; then
+        echo "create database IF NOT EXISTS HlPrintSvc" | mysql -uroot -p$ROOT_PASSWD
+        echo "source /home/initsql/HlPrintSvc.sql" | mysql -uroot -p$ROOT_PASSWD  HlPrintSvc
+        sleep 5
+    fi
+    break
+done
+echo "finish check mysql"
