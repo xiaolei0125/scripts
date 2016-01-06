2edf32032453a279a841f2e46f12901bbcba92b8
FrankieXiao <FrankieXiao@s3graphics.com>
2016-01-06 20:45:28 +0800

    refine useful_cmd.sh

--- useful_cmd.sh
+++ useful_cmd.sh
@@ -1,3 +1,6 @@
 mount.cifs  //10.3.44.23/frankie/E2UMA_Android_Kernel_3.0.36  /work-dbg/mount_android_kernel_3.0.36/   -o username=root,password='root'
 mount.cifs //hz2.byodwork.cn/image /mnt  -o port=4451 username=root,password='123456'
 
+# -d means use post, 
+curl -d "topic=/mytopic" --data-urlencode "message=fuck8844 hi" http://192.168.0.15:8100/pub
+
